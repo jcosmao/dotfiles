0bd5851d34f6c9b6c9d267d665460e3dfa7db3f3
jcosmao <j.cosmao@gmail.com>
2017-12-20 20:12:33 +0100

    i3 conf

--- i3/scripts/i3lock_blur.sh
+++ i3/scripts/i3lock_blur.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-image_file=/tmp/screen_lock.png
+image_file="/tmp/$(basename $0).png"
+rm -f $image_file
 resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
 #filters='noise=alls=10,scale=iw*.05:-1,scale=iw*20:-1:flags=neighbor'
 filters='boxblur=20'
 
 ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -vframes 1 -vf "$filters" "$image_file"
 DISPLAY=:0.0 i3lock -t -e -i "$image_file"
-rm $image_file
+chmod 777 $image_file
+
+exit 0
--- i3/scripts/i3lock_pix.sh
+++ i3/scripts/i3lock_pix.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-tmpbg='/tmp/screen.png'
+tmpbg="/tmp/$(basename $0).png"
 
+rm -f "$tmpbg"
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 i3lock -i "$tmpbg"
-rm "$tmpbg"
+chmod 777 $tmpbg
+
+exit 0
