ac4c8dfc3db6d6016ab1930299879c46c1e5e167
julien.cosmao <julien.cosmao@corp.ovh.com>
2020-11-26 19:58:00 +0100

    ctags

---
+++ vim/plugin/fzf.preview.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eEuCo pipefail
+
+declare -r file=$1
+declare -i -r line=$2
+declare -i -r lines=$LINES
+
+# subtract 3 for the header
+declare -i center=$(( (lines - 3) / 2 ))
+
+if [ $line -lt $center ]; then
+    center=$line
+fi
+declare -i -r start=$(( line - center ))
+declare -i -r end=$(( lines + start ))
+
+exec bat --color always --highlight-line $line --line-range $start:$end --paging never "$file"
