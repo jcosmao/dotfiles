b9429ef1d1e7c2f091814b0725acafc0123d871c
julien.cosmao <julien.cosmao@ovhcloud.net>
2021-12-29 12:09:14 +0100

    fix

--- vim/lua/config/lspconfig.lua
+++ vim/lua/config/lspconfig.lua
@@ -74,7 +74,7 @@ end
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
-    local opts = {}
+    local opts = {on_attach=on_attach}
 
     -- (optional) Customize the options passed to the server
     -- if server.name == "tsserver" then
