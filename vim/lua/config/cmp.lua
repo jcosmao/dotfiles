df2719f79d9755e88f0f2f49e5565d4c96139920
julien.cosmao <julien.cosmao@ovhcloud.net>
2022-10-17 10:31:20 +0200

    fix

--- vim/lua/config/cmp.lua
+++ vim/lua/config/cmp.lua
@@ -95,7 +95,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 for _, lspobj in pairs(vim.lsp.get_active_clients()) do
     lspconfig[lspobj.name].setup {
         capabilities = capabilities
