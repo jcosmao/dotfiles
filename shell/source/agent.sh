387726ed39ce55ac70e52b0c755df9ee90fa2430
julien.cosmao <julien.cosmao@ovhcloud.net>
2023-07-04 13:29:14 +0200

    ssh agent

--- shell/source/agent.sh
+++ shell/source/agent.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-export SSH_AUTH_SOCK=~/.cache/ssh-agent.sock
 # prompt for GPG passphrase when needed
 export GPG_TTY=$(tty)
 
-function launch_ssh_agent ()
+function launch_ssh_agent
 {
-    ssh_agent_cmd="ssh-agent -s -t 36000 -a ${SSH_AUTH_SOCK}"
-
-    if [[ -f ~/.ssh-agent ]]; then
-        eval "$(<~/.ssh-agent)" >/dev/null
+    if [[ -n $SSH_TTY && -n $SSH_AUTH_SOCK ]]; then
+        # SSH agent forwarded, nothing to spawn
+        return
     fi
 
-    if env | grep -q SSH_AGENT_PID=; then
+    # cache key for 10h
+    ssh_agent_cmd="ssh-agent -s -t 36000"
+    eval "$(<~/.ssh-agent)" >/dev/null
+
+    if [[ -n $SSH_AGENT_PID ]]; then
         cmd=$(ps --no-headers -o cmd $SSH_AGENT_PID)
         if [[ $ssh_agent_cmd != $cmd ]]; then
             kill $SSH_AGENT_PID &> /dev/null
         fi
     fi
 
-    output=$(ssh-add -l 2> /dev/null)
-    rc=$?
-    # Seems no ssh-agent running
+    output=$(ssh-add -l 2> /dev/null); rc=$?
+    # no ssh-agent running
     if [[ $rc -eq 2 ]]; then
         # Try to export ssh-agent env
-        rm -f $SSH_AUTH_SOCK
-        (umask 066; ssh-agent -s -t 36000 -a ${SSH_AUTH_SOCK} > ~/.ssh-agent)
+        (umask 066; ssh-agent -s -t 36000 > ~/.ssh-agent)
         eval "$(<~/.ssh-agent)" >/dev/null
         ssh-add 2> /dev/null
     elif [[ $rc -eq 1 ]]; then
