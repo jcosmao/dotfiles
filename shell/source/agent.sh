788d3fd26892f3c47de4f85de360eee9971b51b9
Julien COSMAO <julien.cosmao@ovhcloud.com>
2024-05-23 09:28:18 +0200

    ssh agent

--- shell/source/agent.sh
+++ shell/source/agent.sh
@@ -16,30 +16,30 @@ function launch_ssh_agent
 
     # check current agent
     if [[ -n $agent_pid ]]; then
-        cmd=$(ps --no-headers -o cmd $agent_pid)
-        if [[ $ssh_agent_cmd != $cmd ]]; then
-            kill $agent_pid &> /dev/null
+        cmd=$(ps --no-headers -o cmd "$agent_pid")
+        if [[ $ssh_agent_cmd != "$cmd" ]]; then
+            kill "$agent_pid" &> /dev/null
         fi
     fi
 
     eval "$(cat ~/.ssh-agent 2> /dev/null)" >/dev/null
     if [[ -n $SSH_AGENT_PID ]]; then
-        cmd=$(ps --no-headers -o cmd $SSH_AGENT_PID)
-        if [[ $ssh_agent_cmd != $cmd ]]; then
-            kill $SSH_AGENT_PID &> /dev/null
+        cmd=$(ps --no-headers -o cmd "$SSH_AGENT_PID")
+        if [[ $ssh_agent_cmd != "$cmd" ]]; then
+            kill "$SSH_AGENT_PID" &> /dev/null
         fi
     fi
 
-    output=$(ssh-add -l 2> /dev/null); rc=$?
+    ssh-add -l 2> /dev/null; rc=$?
     # no ssh-agent running
     if [[ $rc == 2 ]]; then
         # Try to export ssh-agent env
         (umask 066; eval "$ssh_agent_cmd" >| ~/.ssh-agent)
         eval "$(<~/.ssh-agent)" >/dev/null
-        ssh-add 2> /dev/null
+        ssh-add $(ls ~/.ssh/*.pub | sed -re 's/\.pub$//') 2> /dev/null
     elif [[ $rc == 1 ]]; then
         # Add default ssh-key to agent cache
-        ssh-add 2> /dev/null
+        ssh-add $(ls ~/.ssh/*.pub | sed -re 's/\.pub$//') 2> /dev/null
     else
         return
     fi
