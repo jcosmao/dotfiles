c8900849cf4b37fad4b16b3e450f79c0fd8aa0cf
Julien COSMAO <julien.cosmao@ovhcloud.com>
2024-10-11 11:15:57 +0200

    gitsecret

--- shell/source/git_secret.sh
+++ shell/source/git_secret.sh
@@ -1,9 +1,11 @@
-export GIT_SECRET_DIR="/dev/shm/secrets"
+export GIT_SECRET_ROOT="/dev/shm/secrets"
 
 function gs
 {
+    [[ $# -eq 0 ]] && cd $GIT_SECRET_ROOT
+
     [[ -z $SECRETS_DIR ]] && echo "Missing env SECRETS_DIR. use gs.set_secret" && return
-    git secret $*
+    git -C $GIT_SECRET_ROOT secret $*
 }
 
 function gs.set_secret
@@ -12,15 +14,21 @@ function gs.set_secret
     export SECRETS_DIR=".gitprivate/$secret_name"
 }
 
-function gs.list_secrets
+function gs.list_env
 {
-    [[ -d $GIT_SECRET_DIR ]] && ls ${GIT_SECRET_DIR}/.gitprivate
+    [[ -d $GIT_SECRET_ROOT ]] && ls ${GIT_SECRET_ROOT}/.gitprivate
 }
 
 function _complete_gs_secrets
 {
     local word=${COMP_WORDS[1]}
-    COMPREPLY=($(compgen -W "$(gs.list_secrets | xargs)" -- ${word}))
+    COMPREPLY=($(compgen -W "$(gs.list_env | xargs)" -- ${word}))
 }
 
 complete -F _complete_gs_secrets gs.set_secret
+
+function gs.gpg
+{
+    [[ -z $SECRETS_DIR ]] && echo "Missing env SECRETS_DIR. use gs.set_secret" && return
+    gpg --homedir ${GIT_SECRET_ROOT}/${SECRETS_DIR}/keys/ $*
+}
