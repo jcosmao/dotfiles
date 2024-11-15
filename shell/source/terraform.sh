02fd883d0d546f73537143caeb7954fe6c30d9c2
Julien COSMAO <julien.cosmao@ovhcloud.com>
2024-11-15 17:20:00 +0100

    fix

--- shell/source/terraform.sh
+++ shell/source/terraform.sh
@@ -19,7 +19,7 @@ function tf
 
     [[ -z $TF_DIR ]] && unset TF_DIR || export TF_WORKSPACE=$TF_DIR
 
-    echo "│ Set WORKSPACE to $TF_WORKSPACE"
+    >&2 echo "│ Set WORKSPACE to $TF_WORKSPACE"
 
     state_dir="terraform.tfstate.d/${TF_WORKSPACE}"
     mkdir -p $state_dir
@@ -32,7 +32,7 @@ function tf
         fi
 
         if [[ -n $TF_COMMAND && "$*" != "${TF_COMMAND}" ]]; then
-            echo "│ WORKSPACE=$TF_WORKSPACE  previously used with args: '$TF_COMMAND'"
+            >&2 echo "│ WORKSPACE=$TF_WORKSPACE  previously used with args: '$TF_COMMAND'"
             unset response
             [[ $SHELL =~ zsh ]] && vared -p '│ continue ? [y] ' -c response || read -r -p '│ continue ? [y] ' response
             if [[ $response != y ]]; then
@@ -42,7 +42,7 @@ function tf
         fi
 
         if [[ -n $TF_OS_REGION_NAME && $OS_REGION_NAME != $TF_OS_REGION_NAME ]]; then
-            echo "│ OS_REGION_NAME is not set to = $TF_OS_REGION_NAME "
+            >&2 echo "│ OS_REGION_NAME is not set to = $TF_OS_REGION_NAME "
             unset response
             [[ $SHELL =~ zsh ]] && \
                 vared -p '│ continue ? [y] ' -c response || \
@@ -63,7 +63,7 @@ function tf
     fi
 
     if [[ $1 = "destroy" ]]; then
-        echo -n "│ Rm tfstate if empty: "
+        >&2 echo -n "│ Rm tfstate if empty: "
         cat ${state_dir}/terraform.tfstate | jq -e '.resources | length == 0' && rm -rf ${state_dir}
         unset TF_WORKSPACE
     fi
@@ -95,6 +95,7 @@ alias tfy="tf auto-approve"
 alias tfset=terraform.set_workspace
 alias tfunset=terraform.unset_workspace
 alias tg=terragrunt
+alias tfo="tf output --json | jq .all.value"
 
 complete -C $(which terraform) terraform
 complete -C $(which terraform) tf
